bcffbd8c94386738069296ba4c3455e635d5e989
ilyaNEW <81633348+nesterovilya111@users.noreply.github.com>
2021-12-19 02:07:44 -0300

    second

--- src/App.js
+++ src/App.js
@@ -1,23 +1,131 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BinarySearchTree } from './utils/BinarySearchTree';
+
+const bst = new BinarySearchTree();
+
+function Tree(props) {
+  const { data } = props;
+  function onClick(num) {
+    alert(num);
+  }
+  function renderTree(node) {
+    return Object.entries(node).map(function ([key, value]) {
+      if (key === 'left' && typeof value === 'object') {
+        if (value !== null) {
+          return (
+            <li
+              key={value.data}
+              onClick={() => {
+                onClick(value.data);
+              }}
+            >
+              <Tree data={value} />
+            </li>
+          );
+        }
+        if (value === null) {
+          return null;
+        }
+      } else if (key === 'right' && typeof value === 'object') {
+        if (value !== null) {
+          return (
+            <li key={value.data}>
+              <Tree data={value} />
+            </li>
+          );
+        }
+        if (value === null) {
+          return null;
+        }
+      }
+    });
+  }
+
+  return data ? (
+    <>
+      <span className="tf-nc">{data.data}</span>
+      <ul>{renderTree(data)}</ul>
+    </>
+  ) : (
+    'EMPTY'
+  );
+}
 
 function App() {
+  const [number, setNumber] = React.useState(50);
+  const [root, setRoot] = React.useState(null);
+
+  React.useEffect(() => {
+    bst.add(50);
+    bst.add(45);
+    bst.add(55);
+    bst.add(53);
+    bst.add(54);
+    bst.add(51);
+    bst.add(40);
+    bst.add(48);
+    bst.add(60);
+    bst.add(30);
+    bst.add(80);
+    bst.add(10);
+    bst.add(42);
+    bst.add(32);
+    bst.add(58);
+    setRoot((prev) => ({ ...prev, ...bst.root }));
+  }, []);
+
+  function changeNumber(e) {
+    setNumber(Number(e.target.value));
+  }
+
+  function addNumber(e) {
+    e.preventDefault();
+    bst.add(number);
+    setRoot((prev) => ({ ...prev, ...bst.root }));
+  }
+
+  function removeNumber(e) {
+    e.preventDefault();
+    bst.remove(number);
+    setRoot((prev) => ({ ...prev, ...bst.root }));
+  }
+
+  function findMax() {
+    alert(`Max value is ${bst.findMax()}`);
+  }
+
+  function findMin() {
+    alert(`Mim value is ${bst.findMin()}`);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Binary Search Tree Visualization</h1>
+      <form onSubmit={addNumber} className="form-control">
+        <input type="number" min="1" name="add" onChange={changeNumber} required />
+        <button type="submit" className="input-button">
+          Add
+        </button>
+        <button type="button" onClick={findMax} className="find-max-button">
+          Find Max
+        </button>
+      </form>
+      <form onSubmit={removeNumber} className="form-control">
+        <input type="number" min="1" name="remove" onChange={changeNumber} required />
+        <button type="submit" className="input-button">
+          Remove
+        </button>
+        <button type="button" onClick={findMin} className="find-min-button">
+          Find Min
+        </button>
+      </form>
+      <div className="tf-tree tf-custom">
+        <ul>
+          <li>
+            <Tree data={root} parent={bst.root} />
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
